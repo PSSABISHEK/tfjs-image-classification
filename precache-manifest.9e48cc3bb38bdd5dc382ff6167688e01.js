433de3679bb443a58d8f7b24b41322ae81d45c21
P.S.S.ABISHEK <pssabishek18@gmail.com>
2020-05-12 10:12:55 +0530

    Updates

---
+++ precache-manifest.9e48cc3bb38bdd5dc382ff6167688e01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a81f6557ad08f0b4f991aee4ba7abc",
+    "url": "/alphaml-iosdebug/index.html"
+  },
+  {
+    "revision": "2c566f5ca0fede5e4694",
+    "url": "/alphaml-iosdebug/static/js/2.d1210266.chunk.js"
+  },
+  {
+    "revision": "b6353da42e1f980c4626",
+    "url": "/alphaml-iosdebug/static/js/main.89052f4f.chunk.js"
+  },
+  {
+    "revision": "fb925b19f6733343d0ca",
+    "url": "/alphaml-iosdebug/static/js/runtime-main.6ba6c360.js"
+  }
+]);
